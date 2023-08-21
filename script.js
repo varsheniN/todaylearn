c63a04f7a4c99293497c961a62e2d45ce571953d
varsheniN <varshanithi23@gmail.com>
2023-08-21 21:36:52 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("hello world!");
+const btn = document.querySelector(".btn-open");
+const form = document.querySelector(".fact-form");
+const factslist = document.querySelector(".facts-list");
+factslist.innerHTML = "";
+
+btn.addEventListener("click", function () {
+  if (form.classList.contains("hidden")) {
+    form.classList.remove("hidden");
+    btn.textContent = "Close";
+  } else {
+    form.classList.add("hidden");
+    btn.textContent = "Share a fact";
+  }
+});
